9ef98a7b77800de90fc64b6e4be08e7679d5f907
chetanyadav95 <chetanyadav12213@gmail.com>
2020-12-19 02:42:08 +0530

    update content and add gold image

--- src/app/funded-program/funded-program.service.ts
+++ src/app/funded-program/funded-program.service.ts
@@ -5,36 +5,36 @@ export class FundedProgramService {
 
   private challenges = [
     {
-      initialCapital: 'Rs. 1,00,000',
+      initialCapital: '5,00,000 INR',
       tradingStrategy: 'Normal',
       progitTarget: '5%',
       progitTargetCriteria: '(Funded account 2.5%)',
       instruments: 'ALL*',
       tradingPeriod: '30 trading days**',
       yourShare: '40 - 80%',
-      evaluationFee: '40 - 80%',
+      evaluationFee: '1500 INR',
       tnc: 'No monthly fees, the choosen retail broker is offering trading days = business days'
     },
     {
-      initialCapital: 'Rs. 1,00,000',
+      initialCapital: 'Rs. 25,00,000',
       tradingStrategy: 'Normal',
       progitTarget: '5%',
       progitTargetCriteria: '(Funded account 2.5%)',
       instruments: 'ALL*',
       tradingPeriod: '30 trading days**',
       yourShare: '40 - 80%',
-      evaluationFee: '40 - 80%',
+      evaluationFee: '7500 INR',
       tnc: 'No monthly fees, the choosen retail broker is offering trading days = business days'
     },
     {
-      initialCapital: 'Rs. 1,00,000',
+      initialCapital: '50,00,000 INR',
       tradingStrategy: 'Normal',
       progitTarget: '5%',
       progitTargetCriteria: '(Funded account 2.5%)',
       instruments: 'ALL*',
       tradingPeriod: '30 trading days**',
       yourShare: '40 - 80%',
-      evaluationFee: '40 - 80%',
+      evaluationFee: '15000 INR',
       tnc: 'No monthly fees, the choosen retail broker is offering trading days = business days'
     }
   ];
